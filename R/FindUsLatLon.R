ddaf1cf63c7c308bda0cdcfac4baab2b8a7164db
jpfonseca84 <jpfonseca@gmail.com>
2017-05-03 12:33:55 -0400

    Removed the library("zipcode") obligatory load to a require("zipcode")

--- R/FindUsLatLon.R
+++ R/FindUsLatLon.R
@@ -1,23 +1,23 @@
-#'This function allows you to find the latitude and longitude based on
-#'US Zip code. A vector can be provided to the function.
+#'This simple function receives a vector of ZIP codes of any length and
+#'it returns a df with zip, latitude and longitude.
 
 findUSLatLon <- function(qzip = NULL) {
       if (is.null(qzip)) {
             stop("Please provide a zip vector")
       }
       #opening package with zipcodes and Lat/Long
-      library("zipcode")
+      require("zipcode")
       data("zipcode")
 
       #preparing the answer
-      nzip<-length(qzip)
-      answer<-data.frame()
-      for (i in 1:nzip){
-            tempset<-subset(zipcode,subset = zipcode$zip==qzip[i])
-            answer[i,1]<-tempset$zip
-            answer[i,2]<-tempset$latitude
-            answer[i,3]<-tempset$longitude
+      nzip <- length(qzip)
+      answer <- data.frame()
+      for (i in 1:nzip) {
+            tempset <- subset(zipcode, subset = zipcode$zip == qzip[i])
+            answer[i, 1] <- tempset$zip
+            answer[i, 2] <- tempset$latitude
+            answer[i, 3] <- tempset$longitude
       }
-      names(answer)<-c("zip","Lat","Lon") #rename columns
+      names(answer) <- c("zip", "Lat", "Lon") #rename columns
       return(answer)
 }
